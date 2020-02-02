9cfaeac112d004f9115f2cf8b8e6654dd2c68aa9
Santiago Medina <santiago@timba.co>
2020-02-02 13:37:51 -0500

    first commit

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,155 @@
+package com.example.android.guesstheword.screens.game
+
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
+private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
+private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
+private val NO_BUZZ_PATTERN = longArrayOf(0)
+
+class GameViewModel: ViewModel() {
+
+    companion object {
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 60000L
+
+        // This is the time when the phone will start buzzing each second
+        private const val COUNTDOWN_PANIC_SECONDS = 10L
+    }
+
+    enum class BuzzType(val pattern: LongArray) {
+        CORRECT(CORRECT_BUZZ_PATTERN),
+        GAME_OVER(GAME_OVER_BUZZ_PATTERN),
+        COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
+        NO_BUZZ(NO_BUZZ_PATTERN)
+    }
+
+    private  val timer : CountDownTimer
+    private var _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+    get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime){time-> DateUtils.formatElapsedTime(time)}
+    // The current word
+    private var _word = MutableLiveData<String>()
+    val word :LiveData<String>
+    get() = _word
+
+    // The current score
+    private var _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
+
+    private var _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+    get() = _eventGameFinish
+
+    private var _eventBuzz = MutableLiveData<BuzzType>()
+    val eventBuzz: LiveData<BuzzType>
+    get() = _eventBuzz
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    init {
+        Log.i("GameModelView","GameViewModel created!")
+        resetList()
+        nextWord()
+        timer = object: CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _eventBuzz.value = BuzzType.GAME_OVER
+                _eventGameFinish.value = true
+            }
+
+            override fun onTick(time: Long) {
+               _currentTime.value = time/ ONE_SECOND
+                if (time / ONE_SECOND <= COUNTDOWN_PANIC_SECONDS) {
+                    _eventBuzz.value = BuzzType.COUNTDOWN_PANIC
+                }
+            }
+
+        }
+        timer.start()
+        _score.value = 0
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameModelView","GameViewModel onCleared!”")
+        timer.cancel()
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            resetList()
+        } else {
+            _word.value = wordList.removeAt(0)
+        }
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        _score.value = _score.value?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = _score.value?.plus(1)
+        _eventBuzz.value = BuzzType.CORRECT
+        nextWord()
+    }
+
+    fun onGameFinishComplete() {
+        _eventGameFinish.value= false
+    }
+
+    fun onBuzzComplete(){
+        _eventBuzz.value = BuzzType.NO_BUZZ
+    }
+}
